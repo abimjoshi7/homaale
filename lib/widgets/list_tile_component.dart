195192c690a9165e3ea2082cc00e3531da2be94b
ambika <ambika>
2023-03-16 17:54:30 +0545

    fix : design improvement

--- lib/widgets/list_tile_component.dart
+++ lib/widgets/list_tile_component.dart
@@ -32,23 +32,38 @@ class ListTileComponent extends StatelessWidget {
           child: Image.network(userImage),
         ),
         title: Padding(
-          padding: const EdgeInsets.only(top: 8.0,),
-          child: Text("$userName $statusDetails"),
+          padding: const EdgeInsets.only(top: 16.0, left: 8),
+          child: RichText(
+            text: TextSpan(
+              text: userName,
+              style: DefaultTextStyle.of(context).style,
+              children: <TextSpan>[
+                TextSpan(
+                  text: " $statusDetails",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
         ),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             if (statusTitle == "status completed" || statusTitle == "rejected")
               TextButton(
-                onPressed: () {},
-                child: const Text("Browse New Task",style: TextStyle(color: Colors.blue),
+                onPressed: () {
+                },
+                child: const Text(
+                  "Browse New Task",
+                  style: TextStyle(color: Colors.blue),
                 ),
               ),
             if (time != null)
               Padding(
                 padding: const EdgeInsets.only(left: 8.0),
-                child: Text(getVerboseDateTimeRepresentation(time!)),
+                child: Text(
+                  getVerboseDateTimeRepresentation(time!),
+                ),
               )
             else
               const SizedBox(),
@@ -58,10 +73,30 @@ class ListTileComponent extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 5),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
-            color: statusTitle == "status completed" ? Colors.green :Colors.red,
+            color: statusTitle == "status completed" ||
+                    statusTitle == "payment completed"
+                ? Colors.green
+                : statusTitle == "approval" || statusTitle == "Approved"
+                    ? Colors.teal
+                    : statusTitle == "booking"
+                        ? Colors.blue
+                        : statusTitle == "waiting"
+                            ? Colors.orange
+                            : Colors.red,
           ),
           child: Text(
-            statusTitle,
+            (statusTitle == "status completed" ||
+                    statusTitle == "payment completed")
+                ? "Completed"
+                : (statusTitle == "approval")
+                    ? "Approval"
+                    : (statusTitle == "waiting")
+                        ? "Waiting"
+                        : (statusTitle == "rejected")
+                            ? "Rejected"
+                            : (statusTitle == "booking")
+                                ? "Booking"
+                                : statusTitle,
             style: const TextStyle(color: Colors.white),
           ),
         ),
