045b72d4c58dda4b852e50a89ce99881dc728911
ambika <ambika>
2023-03-20 12:58:39 +0545

    add : payment methods type  repo added

--- lib/features/payment/data/repositories/payment_repositories.dart
+++ lib/features/payment/data/repositories/payment_repositories.dart
@@ -1,21 +1,15 @@
-
 import 'package:cipher/core/cache/cache_helper.dart';
 import 'package:cipher/core/dio/dio_helper.dart';
 
-
 class PaymentRepositories {
   final _dio = DioHelper();
-
-
   Future<Map<String, dynamic>> postPaymentIntent(
-      String provider,
-      String uuid,
-      ) async {
+    String provider,
+    String uuid,
+  ) async {
     try {
       final res = await _dio.postDataWithCredential(
-        data: {
-          'order':uuid
-        },
+        data: {'order': uuid},
         url: "payment/intent/$provider/",
         token: CacheHelper.accessToken,
       );
@@ -25,4 +19,15 @@ class PaymentRepositories {
     }
   }
 
+  Future<Map<String, dynamic>> getPaymentTypeList() async {
+    try {
+      final res = await _dio.getDatawithCredential(
+        url: 'payment/cms/payment-method/',
+        token: CacheHelper.accessToken,
+      );
+      return res as Map<String, dynamic>;
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
