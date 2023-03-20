5a6c19b548fb02024f367852d050d431ddb88d99
ambika <ambika>
2023-03-20 12:57:51 +0545

    fix: payment methods display common widget

--- lib/features/payment/presentation/pages/widgets/common_wallet_card.dart
+++ lib/features/payment/presentation/pages/widgets/common_wallet_card.dart
@@ -1,42 +1,41 @@
-import 'package:cipher/features/payment/presentation/pages/payment_page.dart';
 import 'package:flutter/material.dart';
 
-class CommonWalletDisplayCard extends StatefulWidget {
+class CommonWalletDisplayCard extends StatelessWidget {
   final String walletAssets;
   final String walletName;
   final String walletCardNo;
   final Widget radio;
-  const CommonWalletDisplayCard({
-    super.key,
-    required this.walletAssets,
-    required this.walletName,
-    required this.walletCardNo,
-    required this.radio,
-  });
 
-  @override
-  State<CommonWalletDisplayCard> createState() =>
-      _CommonWalletDisplayCardState();
-}
+  const CommonWalletDisplayCard(
+      {Key? key,
+      required this.walletAssets,
+      required this.walletName,
+      required this.walletCardNo,
+      required this.radio})
+      : super(key: key);
 
-class _CommonWalletDisplayCardState extends State<CommonWalletDisplayCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-        child: ListTile(
-          leading: Image.network(
-            widget.walletAssets,
-            height: 100,
-            width: 100,
+    return SizedBox(
+      height: 100,
+      child: Card(
+        margin: EdgeInsets.all(5),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+          child: ListTile(
+            leading: Image.network(
+              walletAssets,
+              height: 100,
+              width: 100,
+              scale: 1,
+            ),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [Text(walletName), Text(walletCardNo)],
+            ),
+            trailing: radio,
           ),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [Text(widget.walletName), Text(widget.walletCardNo)],
-          ),
-          trailing: widget.radio,
         ),
       ),
     );
