81db1f0fb85a61b69b7b5f9ebd82469c139243e2
ambika <ambika>
2023-03-26 14:20:51 +0545

    fix: minor ui fixes

--- lib/features/box/presentation/pages/box.dart
+++ lib/features/box/presentation/pages/box.dart
@@ -1,9 +1,14 @@
 import 'package:cipher/core/constants/constants.dart';
+import 'package:cipher/features/box/presentation/bloc/order_id_create_event.dart';
 import 'package:cipher/features/box/presentation/bloc/order_item_list_bloc.dart';
 import 'package:cipher/features/box/presentation/bloc/order_item_list_event.dart';
 import 'package:cipher/features/box/presentation/bloc/order_item_list_state.dart';
 import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
+
+import '../../../bookings/presentation/widgets/sections/services/services_section.dart';
+import '../../../checkout/presentation/pages/checkout_page.dart';
+import '../bloc/order_id_create_bloc.dart';
 
 class BoxPage extends StatefulWidget {
   const BoxPage({super.key});
@@ -31,7 +36,7 @@ class _BoxPageState extends State<BoxPage> with TickerProviderStateMixin {
   }
 
   final TextStyle style = TextStyle(color: Colors.black, fontSize: 14);
-  List<int> selectedItems = [0];
+  List<int> selectedItems = [];
   int current = 0;
   int sum = 0;
   @override
@@ -52,6 +57,7 @@ class _BoxPageState extends State<BoxPage> with TickerProviderStateMixin {
       ),
       body: BlocBuilder<OrderItemListBloc, OrderItemListState>(
         builder: (context, state) {
+          print(_tabController.index);
           return Column(
             children: [
               TabBar(
@@ -80,7 +86,6 @@ class _BoxPageState extends State<BoxPage> with TickerProviderStateMixin {
                         // });
                         return GestureDetector(
                           onTap: () async {
-                            // current = await selectedItems[index];
                             setState(() {
                               if (selectedItems.contains(index)) {
                                 selectedItems.remove(index);
@@ -88,6 +93,7 @@ class _BoxPageState extends State<BoxPage> with TickerProviderStateMixin {
                                 selectedItems.add(index);
                               }
                             });
+                            current = index;
                           },
                           child: Container(
                             decoration: BoxDecoration(
@@ -314,75 +320,88 @@ class _BoxPageState extends State<BoxPage> with TickerProviderStateMixin {
                         );
                       },
                     ),
-                    Container(),
+                    ServicesSection(isCheckPending: true),
                   ],
                 ),
               ),
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 100.0,
-                    top: 20,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Divider(
-                        height: 3,
-                        thickness: 2,
-                        color: Color(0Xff00D084),
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 20.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Total Price',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Rs  '
-                                    '${state.orderItemList?.result![current].entityService?.budgetTo.toString()}',
-                                    // '${sum.toString()}',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.all(20),
-                              padding: EdgeInsets.all(12),
-                              decoration: BoxDecoration(
-                                color: Color(0Xff00D084),
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Text(
-                                'Proceed to Payment ',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 1),
-                              ),
-                            ),
-                          ],
+              if (selectedItems.isNotEmpty)
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 100.0,
+                      top: 20,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Divider(
+                          height: 3,
+                          thickness: 2,
+                          color: Color(0Xff00D084),
                         ),
-                      ),
-                    ],
+                        GestureDetector(
+                          onTap: () {
+                            context.read<OrderIdCreateBloc>().add(
+                                  OrderIdCreateInitiated(
+                                      uuid: state.orderItemList
+                                              ?.result![current].id ??
+                                          ""),
+                                );
+                            Navigator.pushNamed(
+                              context,
+                              CheckoutPage.routeName,
+                            );
+                            // print('${state.orderItemList?.result![current].id}');
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Total Price',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      'Rs  '
+                                      '${state.orderItemList?.result![current].entityService?.budgetTo.toString()}',
+                                      // '${sum.toString()}',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.all(20),
+                                padding: EdgeInsets.all(12),
+                                decoration: BoxDecoration(
+                                  color:  Color(0Xff00D084),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                  'Proceed to Payment ',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
             ],
           );
         },
