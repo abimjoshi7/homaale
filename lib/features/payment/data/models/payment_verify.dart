cae760a026d3152fc6b427294a1c6d60d686612e
abim-cagtu <abimjoshi@cagtu.com>
2023-04-05 16:42:02 +0545

    fix:merge fix

--- lib/features/payment/data/models/payment_verify.dart
+++ lib/features/payment/data/models/payment_verify.dart
@@ -1,7 +1,6 @@
-
+// ignore_for_file: invalid_annotation_target
 
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'dart:convert';
 
 part 'payment_verify.freezed.dart';
 part 'payment_verify.g.dart';
@@ -14,16 +13,17 @@ class PaymentVerify with _$PaymentVerify {
     Detail? detail,
   }) = _PaymentVerify;
 
-  factory PaymentVerify.fromJson(Map<String, dynamic> json) => _$PaymentVerifyFromJson(json);
+  factory PaymentVerify.fromJson(Map<String, dynamic> json) =>
+      _$PaymentVerifyFromJson(json);
 }
 
 @freezed
 class Detail with _$Detail {
   const factory Detail({
     String? pidx,
-   @JsonKey(name: 'total_amount') int? totalAmount,
+    @JsonKey(name: 'total_amount') int? totalAmount,
     String? status,
-  @JsonKey(name: 'transaction_id')  dynamic transactionId,
+    @JsonKey(name: 'transaction_id') dynamic transactionId,
     int? fee,
     bool? refunded,
   }) = _Detail;
