7bc4bd5fa8fcddfe98060deae09c51a40fba8a81
ambika <ambika>
2023-04-05 12:54:46 +0545

    fix: module files names fixes

--- lib/features/box/promo_code/data/models/apply_promo_code.dart
+++ lib/features/box/promo_code/data/models/apply_promo_code.dart
@@ -6,11 +6,11 @@ part 'apply_promo_code.freezed.dart';
 part 'apply_promo_code.g.dart';
 
 @freezed
-class ApplyPromoCode with _$ApplyOffer {
+class ApplyPromoCode with _$ApplyPromoCode {
   const factory ApplyPromoCode({
     String? status,
     String? message,
   }) = _ApplyOffer;
 
-  factory ApplyPromoCode.fromJson(Map<String, dynamic> json) => _$ApplyOfferFromJson(json);
+  factory ApplyPromoCode.fromJson(Map<String, dynamic> json) => _$ApplyPromoCodeFromJson(json);
 }
