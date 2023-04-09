aa6d3c09a02fb969163c70b664a8c33da8cf6d00
Ashmit Hamal <ashmithamal@cagtu.com>
2023-04-09 14:41:46 +0545

    fix: payment in box fix

--- lib/features/box/presentation/pages/box.dart
+++ lib/features/box/presentation/pages/box.dart
@@ -6,7 +6,6 @@ import 'package:cipher/features/box/presentation/bloc/order_item_list_event.dart
 import 'package:cipher/features/box/presentation/bloc/order_item_list_state.dart';
 import 'package:cipher/locator.dart';
 import 'package:cipher/widgets/custom_toast.dart';
-import 'package:cipher/widgets/custom_toast.dart';
 import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
 import '../../../bookings/presentation/bloc/bookings_bloc.dart';
