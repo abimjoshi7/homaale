fe4c3b861800b9ffe65a826d292e58b843e76345
abim-cagtu <abimjoshi@cagtu.com>
2023-05-11 14:41:25 +0545

    fix:event throttle on post service/task

--- lib/features/task/presentation/pages/post_task_page.dart
+++ lib/features/task/presentation/pages/post_task_page.dart
@@ -1,7 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
-import 'dart:developer';
-
 import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
 
@@ -909,9 +905,6 @@ class _PostTaskPageState extends State<PostTaskPage> {
                   ),
                 ),
                 baseStyle: Theme.of(context).textTheme.bodySmall,
-                // TextStyle(
-                //   color: Colors.black,
-                // ),
               ),
               clearButtonProps: ClearButtonProps(
                 padding: EdgeInsets.zero,
