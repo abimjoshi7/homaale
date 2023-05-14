4839cd587b9001b4068eeb309bc28de039779964
Ashmit Hamal <ashmithamal@cagtu.com>
2023-05-14 11:35:32 +0545

    fix: widget text fix

--- lib/widgets/widget_text.dart
+++ lib/widgets/widget_text.dart
@@ -26,7 +26,7 @@ class WidgetText extends StatelessWidget {
           kWidth5,
           Text(
             label,
-            maxLines: label.length,
+            maxLines: 1,
             style: Theme.of(context).textTheme.bodySmall,
           ),
         ],
