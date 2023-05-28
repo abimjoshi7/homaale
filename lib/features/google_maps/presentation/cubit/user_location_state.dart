1483b4d515d2d515e6d4c1014020e1913e2a08b1
ijees-cagtu <ljeesshrestha@cagtu.com>
2023-05-28 15:04:28 +0545

    feat: location caching bug fix

--- lib/features/google_maps/presentation/cubit/user_location_state.dart
+++ lib/features/google_maps/presentation/cubit/user_location_state.dart
@@ -1,21 +1,26 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
 part of 'user_location_cubit.dart';
 
 @immutable
 class UserLocationState extends Equatable {
   final LatLng? location;
+  final String? address;
   UserLocationState({
     this.location,
+    this.address,
   });
 
   UserLocationState copyWith({
     LatLng? location,
+    String? address,
   }) {
     return UserLocationState(
       location: location ?? this.location,
+      address: address ?? this.address,
     );
   }
 
   @override
-  List<Object?> get props => [location];
+  List<Object?> get props => [location, address];
 }
