225561d3a9577c2ef1df7e2227a80da43e1f6c98
ambika <ambika>
2023-05-30 10:54:00 +0545

    add: internet checked and apply ui on overall app

--- lib/core/network_info/network_info.dart
+++ lib/core/network_info/network_info.dart
@@ -1,101 +1,18 @@
+
+
 import 'package:dependencies/dependencies.dart';
-import 'dart:async';
-import 'dart:io';
 
-import 'package:flutter/widgets.dart';
+import 'network_bloc.dart';
+import 'network_event.dart';
 
-class DioConnectivityRequestRetrier {
-  final Dio dio;
-  final Connectivity connectivity;
-
-  DioConnectivityRequestRetrier({
-    required this.dio,
-    required this.connectivity,
-  });
-
-  Future<StreamSubscription> scheduleRequestRetry(
-      RequestOptions requestOptions) async {
-    StreamSubscription? streamSubscription;
-
-    streamSubscription = connectivity.onConnectivityChanged.listen(
-      (connectivityResult) async {
-        // We're connected either to WiFi or mobile data
-        if (connectivityResult != ConnectivityResult.none) {
-          Text('No internet Connection Found.');
-          // Ensure that only one retry happens per connectivity change by cancelling the listener
-          streamSubscription?.cancel();
-          // Copy & paste the failed request's data into the new request
-          dio.request(
-            requestOptions.path,
-            cancelToken: requestOptions.cancelToken,
-            data: requestOptions.data,
-            onReceiveProgress: requestOptions.onReceiveProgress,
-            onSendProgress: requestOptions.onSendProgress,
-            queryParameters: requestOptions.queryParameters,
-          );
-        }
-      },
-    );
-    return streamSubscription;
-  }
-}
-
-class RetryOnConnectionChangeInterceptor extends Interceptor {
-  final DioConnectivityRequestRetrier requestRetrier;
-
-  RetryOnConnectionChangeInterceptor({
-    required this.requestRetrier,
-  });
-  Future(DioError err) async {
-    if (_shouldRetry(err)) {
-      try {
-        return requestRetrier.scheduleRequestRetry(err.requestOptions);
-      } catch (e) {
-        // Let any new error from the retrier pass through
-        return e;
+class NetworkHelper {
+  static void observeNetwork() {
+    Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.none) {
+        NetworkBloc().add(NetworkNotify());
+      } else {
+        NetworkBloc().add(NetworkNotify(isConnected: true));
       }
-    }
-    // Let the error pass through if it's not the error we're looking for
-    return err;
-  }
-
-  bool _shouldRetry(DioError err) {
-    return err.type == DioErrorType.connectionError &&
-        err.error != null &&
-        err.error is SocketException;
-  }
-}
-
-class ConnectivityStatus {
-  ConnectivityStatus._();
-
-  static final _instance = ConnectivityStatus._();
-  static ConnectivityStatus get instance => _instance;
-  final _connectivity = Connectivity();
-  final _controller = StreamController.broadcast();
-  Stream get myStream => _controller.stream;
-
-  void initialise() async {
-    ConnectivityResult result = await _connectivity.checkConnectivity();
-    _checkStatus(result);
-    _connectivity.onConnectivityChanged.listen((result) {
-      _checkStatus(result);
     });
   }
-
-  void _checkStatus(ConnectivityResult result) async {
-    bool isOnline = false;
-    try {
-      final result = await InternetAddress.lookup('hommale.com');
-      isOnline = result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-    } on SocketException catch (_) {
-      isOnline = false;
-    }
-
-    if (!_controller.isClosed) {
-      _controller.sink.add({result: isOnline});
-
-    }
-  }
-  void disposeStream() => _controller.close();
 }
--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -4,7 +4,8 @@ import 'package:cipher/widgets/widgets.dart';
 import 'package:dependencies/dependencies.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../core/network_info/network_info.dart';
+import '../../../../core/network_info/network_bloc.dart';
+import '../../../../core/network_info/network_state.dart';
 
 class Home extends StatefulWidget {
   const Home({
@@ -19,41 +20,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Map _source = {ConnectivityResult.wifi: false};
-  final ConnectivityStatus _connectivity = ConnectivityStatus.instance;
   @override
   void initState() {
-    _connectivity.initialise();
-    _connectivity.myStream.listen((source) {
-      setState(() => _source = source as Map);
-    });
     super.initState();
   }
 
   @override
-  void dispose() {
-    _connectivity.disposeStream();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    String string;
-    switch (_source.keys.toList()[0]) {
-      case ConnectivityResult.mobile:
-        string = 'Mobile: Online';
-        break;
-      case ConnectivityResult.wifi:
-        string = 'WiFi: Online';
-        break;
-      case ConnectivityResult.none:
-        string = 'Offline';
-        break;
-      default:
-        string = 'WiFi: Online';
-    }
-    print("object: $string");
-
     return Scaffold(
       body: SizedBox(
         width: double.infinity,
@@ -61,47 +34,41 @@ class _HomeState extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             const HomeHeaderSection(),
-            string == 'Offline'
-                ? NoInternetPage(
-                    assetsPath: 'assets/no_internet_connection.png',
-                    errorTile: 'Oops, No Internet Connection.',
-                    errorDes: "Make sure Wi-Fi or cellular data is turned on and then try again.",
-                  )
-                : Expanded(
-                    child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          bottom: 16,
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            HeaderOfferSection(),
-                            // DealsOfTheDaySection(),
-                            CategoriesSection(),
-                            TrendingServicesSection(),
-                            TasksRecommendationSection(),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Image.asset(
-                                'assets/banners/notable1.png',
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                              ),
-                            ),
-                            PopularTaskerSection(),
-                            SizedBox(height: 10),
-                            NotableBannerSection(),
-                            // RecentlyLaunchedServicesSection(),
-                            TasksAndServicesNearbySection(),
-                            // SizedBox(height: 120),
-                            // const PopularTaskerNearbySection(),
-                            // const TaskRecommendationSection(),
-                            // PopularTaskNearbySection()
-                          ],
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 16,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      HeaderOfferSection(),
+                      // DealsOfTheDaySection(),
+                      CategoriesSection(),
+                      TrendingServicesSection(),
+                      TasksRecommendationSection(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/banners/notable1.png',
+                          fit: BoxFit.cover,
+                          width: double.infinity,
                         ),
                       ),
-                    ),
-                  )
+                      PopularTaskerSection(),
+                      SizedBox(height: 10),
+                      NotableBannerSection(),
+                      // RecentlyLaunchedServicesSection(),
+                      TasksAndServicesNearbySection(),
+                      // SizedBox(height: 120),
+                      // const PopularTaskerNearbySection(),
+                      // const TaskRecommendationSection(),
+                      // PopularTaskNearbySection()
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
